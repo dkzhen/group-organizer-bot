cfa0068baba033f1634434a79098224f55e0b3e9
dkzhen <dani314123@gmail.com>
2024-07-15 16:00:42 +0700

    fix: error send message

--- src/routes/BotRoute.js
+++ src/routes/BotRoute.js
@@ -43,7 +43,8 @@ router.post("/sendMessage", async (req, res) => {
       return res.status(400).json({ message: "field is required" });
     }
     await prisma.token.delete({ where: { id: Number(tokenId) } });
-    bot.sendMessage(chatId, message);
+    await bot.sendMessage(chatId, message);
+    return res.status(200).json({ message: true });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Internal server error" });
