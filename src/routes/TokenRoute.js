8910622495648bbb6fa0c5ab79a092ffc1e9ee0c
dkzhen <dani314123@gmail.com>
2024-07-15 14:25:56 +0700

    fix: json bigint to int

--- src/routes/TokenRoute.js
+++ src/routes/TokenRoute.js
@@ -19,9 +19,16 @@ router.get("/:botid", async (req, res) => {
       return res.status(404).json({ message: `bot token not found` });
     }
 
+    const serializedTokens = tokens.map((token) => ({
+      id: token.id,
+      botId: token.botId,
+      telegramId: Number(token.telegramId), // Konversi BigInt ke Number
+      token: token.token,
+    }));
+
     return res
       .status(200)
-      .json({ message: `bot token ${id} found`, data: botToken });
+      .json({ message: `bot token ${id} found`, data: serializedTokens });
   } catch (error) {
     console.log({ error: "on tokenRoute", message: error });
     return res.status(500).json({ message: "Internal server error" });
